38d3b58b8e7cd942b7adb794c37e36feebe94428
Rajeswaran <rajeswaran149@gmail.com>
2026-05-28 16:47:53 +0530

    update api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL:'http://localhost:5000/api/dashboard'
+    baseURL:'https://data-visualization-dashboard-server-eight.vercel.app/api/dashboard'
+    // baseURL:'http://localhost:5000/api/dashboard'
 })
 
 export default API
